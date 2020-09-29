cb040fa41dbaea614bd28070da7fe63889764926
SeyoungJeon <seyoung0254@hanmial.net>
2020-09-29 16:21:13 +0900

    [Add] test book service

--- book-server/src/main/java/com/navercorp/bookserver/HelloController.java
+++ book-server/src/main/java/com/navercorp/bookserver/HelloController.java
@@ -1,4 +1,13 @@
 package com.navercorp.bookserver;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/")
+    String home(){
+        return "helloo!!!!!";
+    }
 }
