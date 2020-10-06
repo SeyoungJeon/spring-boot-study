dbbdb3002c6388c7be35f7b4943f0e4872b864a4
SeyoungJeon <seyoung0254@hanmial.net>
2020-10-06 12:34:49 +0900

    [Add] 잘못된 책 정보 조회에 대한 예외 응답

--- book-server/src/main/java/com/navercorp/bookserver/BookController.java
+++ book-server/src/main/java/com/navercorp/bookserver/BookController.java
@@ -2,11 +2,13 @@ package com.navercorp.bookserver;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.Objects;
 
-@RestController
+@Controller
 @RequestMapping("/api/books")
 public class BookController {
     private final BookService service;
@@ -22,8 +24,10 @@ public class BookController {
     }
 
     @GetMapping("{id}")
-    Book read(@PathVariable Integer id){
-        return service.findById(id);
+    Book read(@PathVariable Integer id) {
+        Book book = service.findById(id);
+        Objects.requireNonNull(book, "cannot find book with id: " + id);
+        return book;
     }
 
     @PostMapping
--- book-server/src/main/java/com/navercorp/bookserver/BookServerApplication.java
+++ book-server/src/main/java/com/navercorp/bookserver/BookServerApplication.java
@@ -1,13 +1,25 @@
 package com.navercorp.bookserver;
 
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.zalando.problem.ProblemModule;
 
 @SpringBootApplication
 public class BookServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookServerApplication.class, args);
 	}
 
+	@Bean
+	public Module jdk8Module() {
+		return new Jdk8Module();
+	}
+
+	@Bean
+	public Module problemModule() {
+		return new ProblemModule();
+	}
 }
